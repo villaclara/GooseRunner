db0042387c1e9d70ed2f37b8908f15fe0fff233e
villaclaraaa <158094620+villaclaraaa@users.noreply.github.com>
2024-08-30 23:50:47 +0300

    commit

---
+++ Assets/Scripts/Core/Environment/ScreenEdge/ShowSides.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System;
+using UnityEngine;
+
+public class ShowSides : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CanvasGroup canvGroupSpeedUp;
+    public CanvasGroup canvGroupSlowDown;
+
+    private void Start()
+    {
+        OrbsBehaviour.OnSpeedUpOrbPickUp += ShowSideSpeedUp;
+        OrbsBehaviour.OnSlowDownOrbPickUp += ShowSideSlowDown;
+    }
+    public void ShowSideSpeedUp()
+    {
+        StartCoroutine("showSidesCoroutine", canvGroupSpeedUp);
+    }
+
+    public void ShowSideSlowDown()
+    {
+        StartCoroutine("showSidesCoroutine", canvGroupSlowDown);
+    }
+    public IEnumerator showSidesCoroutine(CanvasGroup canvGroup)
+    {
+        while (canvGroup.alpha < 1)
+        {
+            canvGroup.alpha += Time.deltaTime * 5f;
+            yield return null;
+            yield return new WaitUntil(() => !PauseMenu.isPaused);
+        }
+        yield return new WaitForSeconds(1);
+        while (canvGroup.alpha > 0)
+        {
+            canvGroup.alpha -= Time.deltaTime * 5f;
+            yield return null;
+            yield return new WaitUntil(() => !PauseMenu.isPaused);
+        }
+    }
+}
