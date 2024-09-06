cb57244f52dc988270fede908f68ada389f8dbd2
villaclaraaa <158094620+villaclaraaa@users.noreply.github.com>
2024-09-07 01:09:37 +0300

    bugs fixed

--- Assets/Scripts/Core/Environment/AudioManager.cs
+++ Assets/Scripts/Core/Environment/AudioManager.cs
@@ -14,23 +14,53 @@ public class AudioManager : MonoBehaviour
     public AudioClip buttonPressed;
     public AudioClip pannelSlide;
     private int _volumeState;
+    private bool _isNotPlaying;
     private void Start()
     {
+        _isNotPlaying = true;
         _volumeState = PlayerPrefs.GetInt("volumeState", 1);
         ambientSource.clip = background;
         ambientSource.loop = true;
         if (_volumeState == 1)
-            ambientSource.Play();
+        {
+            ambientSource.volume = Mathf.Clamp01(1.0f);
+            SFXSource.volume = Mathf.Clamp01(1.0f);
+        }
+        else
+        {
+            ambientSource.volume = Mathf.Clamp01(0.0f);
+            SFXSource.volume = Mathf.Clamp01(0.0f);
+        }
+
     }
 
     private void Update()
     {
         _volumeState = PlayerPrefs.GetInt("volumeState", 1);
+        if (GameManager.gameStarted && _isNotPlaying)
+        {
+            PlayAmbientMusic();
+            _isNotPlaying = false;
+        }
+        if (_volumeState == 1)
+        {
+            ambientSource.volume = Mathf.Clamp01(1.0f);
+            SFXSource.volume = Mathf.Clamp01(1.0f);
+        }
+        else
+        {
+            ambientSource.volume = Mathf.Clamp01(0.0f);
+            SFXSource.volume = Mathf.Clamp01(0.0f);
+        }
     }
 
     public void PlaySFX(AudioClip clip)
     {
         if(_volumeState == 1)
             SFXSource.PlayOneShot(clip);
+    }
+    public void PlayAmbientMusic()
+    {
+        ambientSource.Play();
     }
 }
